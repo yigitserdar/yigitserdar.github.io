a45c3f7263e905e943d4648ebdb405265b337b71
Serdar Yiğit <yigitserdar34@gmail.com>
2018-01-06 11:42:43 +0300

    update functionality

--- project-pixel-art-maker-starter-master/pixelart_designs.js
+++ project-pixel-art-maker-starter-master/pixelart_designs.js
@@ -1,30 +1,32 @@
-var input_rows;
-var input_cols;
-var input_color = "#000000";
+// Pixel Art Maker script
 
 function makeGrid() {
-  var table = document.getElementById("pixel_canvas");
-  table.innerHTML = "";
-  while(table.rows.length > 0)
-    table.deleteRow(0);
-  input_rows = document.getElementById("input_height").value;
-  input_cols = document.getElementById("input_width").value;
-  for (var i = 0; i < input_rows; i++) {
-    var row_elem = table.insertRow(i);
-    row_elem.setAttribute("class", "row");
-    for (var j = 0; j < input_cols; j++) {
-      var cell= row_elem.insertCell(j);
-      cell.addEventListener('click', function(evt) {
-        evt.target.style.backgroundColor = document.getElementById("colorPicker").value;
-        this.style.borderColor="#9ecaed";
-        this.style.boxShadow="0 0 10px #9ecaed";
-      });
-    }
-  }
-  return false;
+    // Creating variables
+    let rows = $('#input_height').val();
+    let cols = $('#input_width').val();
+    let color;
+
+    // Deleting existing grid
+    $('#pixel_canvas').children().remove();
+
+    // Creating grid
+    for (let i = 0; i < rows; i++) {
+        $('#pixel_canvas').append('<tr></tr>');
+        };
+    for (let j = 0; j < cols; j++) {
+            $('tr').append('<td></<td>');
+        };
+
+    // Fill clicked "pixel"
+    $('td').on('click', function () {
+        color = $('#colorPicker').val();
+        $(this).attr("bgcolor", color);
+    });
 }
 
-document.getElementById("sizePicker").addEventListener("submit", function(evt) {
-  evt.preventDefault();
-  makeGrid();
+// Avoiding default submit function - reload
+// and calling makeGrid function instead
+$("input[type='submit']").click(function (event) {
+    event.preventDefault();
+    makeGrid();
 });
