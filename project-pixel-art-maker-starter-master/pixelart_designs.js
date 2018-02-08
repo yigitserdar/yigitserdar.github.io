3b991e67206f58695b328000146fd8d4897696df
Serdar Yiğit <yigitserdar34@gmail.com>
2018-02-08 16:38:58 +0300

    update pages

--- project-pixel-art-maker-starter-master/pixelart_designs.js
+++ project-pixel-art-maker-starter-master/pixelart_designs.js
@@ -30,3 +30,34 @@ $("input[type='submit']").click(function (event) {
     event.preventDefault();
     makeGrid();
 });
+
+function makeGridRandomly() {
+    // Creating variables
+    let rows = $('#inputHeight').val();
+    let cols = $('#inputWidth').val();
+    let color;
+
+    // Deleting existing grid
+    $('#pixel_canvas').children().remove();
+
+    // Creating grid
+    for (let i = 0; i < rows; i++) {
+        $('#pixel_canvas').append('<tr></tr>');
+        };
+    for (let j = 0; j < cols; j++) {
+            $('tr').append('<td></<td>');
+        };
+
+    // Fill clicked "pixel"
+    $('td').on('click', function () {
+        color = $('#colorPicker').val();
+        $(this).attr("bgcolor", color);
+    });
+}
+
+// Avoiding default submit function - reload
+// and calling makeGrid function instead
+$("input[type='submit']").click(function (event) {
+    event.preventDefault();
+    makeGrid();
+});
